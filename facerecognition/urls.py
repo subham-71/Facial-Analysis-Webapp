86306958c9042d750418383eef7d4aac92fa185a
Subham_subhasis <subhamsubhasis2002@gmail.com>
2023-01-14 13:15:27 +0530

    for vercel deployment

--- facerecognition/urls.py
+++ facerecognition/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('about/',views.about,name='about')
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
